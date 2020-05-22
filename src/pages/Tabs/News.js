4f2d900f69344bad090c5e1040e42815d3149934
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-05-23 00:10:04 +0530

    added News, Profile Pages

---
+++ src/pages/Tabs/News.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+
+const News = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>News</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default News;
---
+++ src/pages/Tabs/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from "@ionic/react";
+
+const News = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Profile</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </IonPage>
+    );
+};
+
+export default News;
