1713671c1de8c96d39ecae2cb58a9d498b8409a4
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-05-23 15:37:09 +0530

    added profile page layout, profilepage.css

--- src/pages/Tabs/Profile.js
+++ src/pages/Tabs/Profile.js
@@ -3,17 +3,54 @@ import {
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonContent,
+    IonAvatar,
+    IonItem,
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
 } from "@ionic/react";
+import '../../theme/variables.css';
+import '../../theme/profilepage.css';
 
 const Profile = () => {
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Profile</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+            <IonContent>
+                <IonHeader className="profile-headline">
+                    <IonToolbar color="headline">
+                        <IonTitle>My Nature Profile</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol size="auto">
+                            <IonAvatar className="avataar-image box">
+                                <img src="https://image.flaticon.com/icons/svg/145/145852.svg" alt="avataar" />
+                            </IonAvatar>
+                        </IonCol>
+                        <IonCol size="6">
+                            <IonItem lines="none">
+                                <IonLabel className="trees-planted">
+                                    <h2> Trees Planted: </h2>
+                                </IonLabel>
+                            </IonItem>
+                            <IonItem lines="none">
+                                <IonLabel className="trees-saved">
+                                    <h2> Trees Saved: </h2>
+                                </IonLabel>
+                            </IonItem>
+                            <IonItem lines="none">
+                                <IonLabel className="trees-cut">
+                                    <h2> Trees Cut: </h2>
+                                </IonLabel>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
         </IonPage>
     );
 };
