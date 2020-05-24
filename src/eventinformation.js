3b3ff91110543667b877ea2b6326f18359f602c5
shahpreetk <preet25@somaiya.edu>
2020-05-24 12:21:40 +0530

    links added to events page

--- src/eventinformation.js
+++ src/eventinformation.js
@@ -6,34 +6,34 @@ export const eventinformation = [
         day:'Friday',
         date:'29th May 2020',
         details:'On this day we plan to plant 500 trees and need volunteers for the same please do register on our website.',
-        link:''
+        link:'https://instagram.com/nss__kjsieit?igshid=17q0qdp020fjv'
     },
     {
         id:2,
-        organization:'Lions Club of Churchgate',
+        organization:'Lions Club International',
         location:'Kalyan, Maharashtra, India',
         day:'Saturday',
         date:'30th May 2020',
         details:'Need 50 volunteers for the event.',
-        link:''
+        link:'https://instagram.com/lionsclubs?igshid=h6c9nnuyoi5b'
     },
     {
         id:3,
-        organization:'Tree Plantation Organization',
+        organization:'Leo Club of Walkeshwar',
         location:'Location of the event',
         day:'Saturday',
         date:'6th June 2020',
         details:'Details/Description of the event',
-        link:''
+        link:'https://instagram.com/leoclub.walkeshwar?igshid=1wgr3vz62krz7'
     },
     {
         id:4,
-        organization:'Leo Club of Walkeshwar',
+        organization:'Tree Plantation Organization',
         location:'Titwala, Maharashtra, India',
         day:'Sunday',
         date:'7th June 2020',
         details:'Trees and tools needed for plantation will be provided with refreshments.',
-        link:''
+        link:'https://instagram.com/leodistrict3231a1?igshid=eruf2chls02x'
     },
     {
         id:5,
@@ -42,6 +42,6 @@ export const eventinformation = [
         day:'Saturday',
         date:'15th June 2020',
         details:'Certificate will be provided to all registered volunteers',
-        link:''
+        link:'https://instagram.com/rc_jaihind?igshid=1p2dy9ba7hvi8'
     }
 ]
